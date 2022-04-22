5251e9037252b01618c50399a9c02062101c770b
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-04-22 14:41:54 +1000

    Add lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use reic::Token;
+
 fn main() {
-    println!("Usage: reic <file.rei>");
+    // 1. handle reic file.rei
+    use std::fs::read_to_string;
+    use std::env;
+
+    // Take in command line args
+    let args: Vec<String> = env::args().collect();
+
+    // Check if there are enough args
+    if args.len() != 1 {
+        println!("Usage: reic <file.rei>");
+        return;
+    }
+
+    // Read the file
+    let file = read_to_string(&args[1]).unwrap();
+
+    // 2. parse the file
+    let tokens = reic::tokenise(&file);
 }
