9c1231da6863591f8541c0b355dfbb4bfc5a0ef7
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-05-20 00:17:06 +1000

    ADD STUFF

--- src/lib.rs
+++ src/lib.rs
@@ -57,3 +57,16 @@ EQUIVALENCE
 */
 
 // NOTE: | means bitwise OR when using numeric. On other types, its free to overload
+
+fn parse(tokens: &[Token]) {
+    for token in tokens {
+        // no backtracking. LR 1
+        // https://en.wikipedia.org/wiki/Canonical_LR_parser#Constructing_LR(1)_parsing_tables
+
+        // https://en.wikipedia.org/wiki/Recursive_descent_parser algorithm
+    }
+}
+
+fn recursive_descent_parser() {
+
+}
