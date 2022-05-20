f6b8d79f79e582cd29fc573e8faaa878dd80b0d7
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-05-20 21:29:13 +1000

    Add stuff

--- docs/GRAMMAR.md
+++ docs/GRAMMAR.md
@@ -2,9 +2,19 @@
 
 Pretty much based on scala. Pest cant parse EBNF since it is PEG. So use PEG instead.
 
+In rei, expressions and statements have a pretty blurry line. The compiler is needs to ensure good things happen.
+
 ## PEG
 
+Note there is an implicit `filename` included in the AST generated. Its basically any valid filename on your OS and reic that doesnt include certain special chars.
+
 ```rust
+content: stmt* 
+
+// -------------
+// STATEMENTS
+// -------------
+
 stmt: function_def | class_def | data_def | variable_def | operator_stmt | if_stmt | for_stmt | while_stmt | return_stmt | trivial_stmt
 
 return_stmt: "return" ~ [expr]
@@ -13,11 +23,61 @@ trivial_stmt: "yield" | "continue" | "break"
 
 variable_def: ("let" ~ ident ~ "=" ~ expr) | ("let" ~ expr ~ "=" ~ expr)
 
+// Control Statements
+
+// should technically be a boolean expr, should be parseable
+if_stmt: {
+  if_block ~ (elif_block* ~ [else_block])
+}
+
+if_block: "if" ~ expr ~ single_or_scoped_block
+
+elif_block: {
+  "elif" ~ expr ~ if_block
+}
+
+else_block: {
+  "else" ~ if_block
+}
+
+for_stmt: {
+  standard_for_stmt | range_based_for_stmt
+}
+
+standard_for_stmt: {
+  "for" ~ for_expr_cond ~ single_or_scoped_block
+}
+
+// for i in start..end
+for_expr_cond: {
+  ident ~ "in" ~ expr ~ ".." ~ expr
+}
+
+single_or_scoped_block: {
+  scoped_block | stmt
+}
+
+// -------------
+// EXPRESSIONS
+// -------------
+
 expr: ["+"|"-"] ~ term ~ {op ~ term}
 
 term: ident | number | "(" expr ")"
 
+// -------------
+// OPERATORS
+// -------------
+
 op: "<" | ...
+
+// -------------
+// SCOPED BLOCKS
+// -------------
+
+scoped_block = {
+  "{" ~ stmt ~ "}"
+}
 ```
 
 ## Types of expressions
--- src/lib.rs
+++ src/lib.rs
@@ -225,6 +225,12 @@ pub fn print_tokens(tokens: &Vec<(Token, std::ops::Range<usize>)>) {
     }
 }
 
+// ----------------
+// PARSING
+// ----------------
+
+use serde_derive::{Deserialize, Serialize};
+
 // https://www.tutorialspoint.com/compiler_design/compiler_design_symbol_table.htm symbol table design
 
 // ident : attr
@@ -242,13 +248,109 @@ struct SymbolTable<'a> {
     symbols: HashMap<&'a str, Namespace<'a>>,
 }
 
-fn parse(tokens: &[Token]) {
-    for token in tokens {
-        // no backtracking. LR 1
-        // https://en.wikipedia.org/wiki/Canonical_LR_parser#Constructing_LR(1)_parsing_tables
+// NOTE: L-attr SDT with inh and synth attr
 
-        // https://en.wikipedia.org/wiki/Recursive_descent_parser algorithm
+// Assumes tree has at least two nodes that is the filename and content
+/// An empty file is a valid program. You can compile and run it. It will simply have a minimal _start and empty main function that returns 0 and links to std if specified
+/// Its like fn main() {}
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct ParseTree {
+    name: String,
+    content: ParseNode<InternalNodeData>,
+}
+
+impl ParseTree {
+    pub fn new(name: String, content: ParseNode<InternalNodeData>) -> Self {
+        Self { name, content }
     }
 }
 
-fn recursive_descent_parser() {}
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct ParseNode<T> {
+    t: T,
+    is_leaf: bool,
+}
+
+impl<T> ParseNode<T> {
+    fn new(t: T, is_leaf: bool) -> Self {
+        Self { t, is_leaf }
+    }
+}
+
+impl ParseNode<InternalNodeData> {
+    pub fn new_internal_node(
+        inherited_attr: String,
+        synthesized_attr: String,
+        children: Vec<InternalNodeData>,
+    ) -> Self {
+        Self {
+            t: InternalNodeData::new(inherited_attr, synthesized_attr, children),
+            is_leaf: false,
+        }
+    }
+}
+
+impl ParseNode<LeafNodeData> {
+    pub fn new_leaf_node(lex_value: String) -> Self {
+        Self {
+            t: LeafNodeData::new(lex_value),
+            is_leaf: true,
+        }
+    }
+}
+
+// should represent a production like T -> T E'
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct InternalNodeData {
+    inherited_attr: String,
+    synthesized_attr: String,
+    children: Vec<InternalNodeData>,
+}
+
+impl InternalNodeData {
+    pub fn new(
+        inherited_attr: String,
+        synthesized_attr: String,
+        children: Vec<InternalNodeData>,
+    ) -> Self {
+        Self {
+            inherited_attr,
+            synthesized_attr,
+            children,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct LeafNodeData {
+    lex_value: String,
+}
+
+impl LeafNodeData {
+    pub fn new(lex_value: String) -> Self {
+        Self { lex_value }
+    }
+}
+
+/*
+COOL PARSING ALGOS:
+ 1. no backtracking. LR 1
+    https://en.wikipedia.org/wiki/Canonical_LR_parser#Constructing_LR(1)_parsing_tables
+ 2. https://en.wikipedia.org/wiki/Recursive_descent_parser algorithm and https:/
+    /www.geeksforgeeks.org/recursive-descent-parser/
+*/
+
+/// Parse those tokens boy
+fn parse(filename: &str, tokens: &[Token]) {
+    // create tree
+    let content_node = ParseNode::new_internal_node("".to_owned(), "".to_owned(), vec![]);
+    let ast = ParseTree::new(filename.to_owned(), content_node);
+
+    for token in tokens {
+        // priority grammar descent
+
+
+    }
+}
+
+
