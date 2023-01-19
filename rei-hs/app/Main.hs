780134e9c3d9e923d20fe5ad4f8e3cec40e2ae7c
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-01-19 12:50:38 +1100

    Update basics

--- rei-hs/app/Main.hs
+++ rei-hs/app/Main.hs
@@ -8,3 +8,54 @@ main = do
 
 stringParser:: Parsec String st String
 stringParser = many anyChar
+
+data GeneralDefModifier = Alias
+                    | Parameterized
+                    | Enum
+                    | Object
+                    | Extend
+                    | Trait
+                    | Impl
+                    | Replace
+                    | Mod
+                    deriving (Eq, Show)
+
+type Generics = [Expr]
+type RefinementExpr = Expr
+type TypeExpr = Expr
+type RawIdentExpr = String
+type IdentifierExpr = Expr
+type UnaryExpr = Expr
+type GeneralDefRhs = Expr
+type ImplRefinement = [Expr]
+type MultipleParamExpr = [Expr]
+data GenericParamType = Dependent | TypeParam deriving (Eq, Show)
+type EnumItem = Expr
+
+data Expr = LiteralExpr Primitive
+          | IdentExpr [String]
+          | BinaryOp OverloableBinaryOperator Expr Expr
+          | UnaryOp OverloableUnaryOperator Expr UnaryType
+          | ParenExpr Expr
+          | GeneralDef RawIdentExpr Generics GeneralDefRhs
+          | Param IdentifierExpr TypeExpr [RefinementExpr]
+          | Refinement UnaryExpr
+          | GenericParam (Maybe RawIdentExpr) IdentifierExpr ImplRefinement GenericParamType
+          | Call Expr Args
+          | EvalExpr Expr
+          | ParameterizedExpr [Expr] ReturnType Expr
+          | Enum [EnumItem]
+          | Complex [Expr]
+          | Trait IdentifierExpr [TraitExpr]
+          | Impl IdentifierExpr [ImplItem]
+          | Annotation Args
+          | MacroBodyExpr Expr
+          | VariableDef TypeModifier Expr Expr
+          | VariableRedef Expr Expr
+          | ExportExpr Module
+          | Require Namespace [ImportItem]
+          | If Expr Expr Expr
+          | Loop LoopType Expr Expr
+          | ScopeExpr [Expr]
+          | ArrowExpr Expr
+          deriving (Eq, Show)
