fd929d3921f6057fb2201ce3096f0271c1808cb3
Jasen Qin <capitalswine@gmail.com>
2023-01-25 18:15:09 +1100

    CFonfusion

--- rei-hs/src/Dataflow.hs
+++ rei-hs/src/Dataflow.hs
@@ -2,4 +2,23 @@ module Dataflow where
 
 data Node = Data | Execute
 
-data AtomicInstruction = Add | Sub | Mul | Div | Copy
+data AtomicInstruction = Add | Sub | Mul | Div | Copy | Run
+
+-- Node is of N bits
+-- all data is represented in N bits, even instructions themselves
+-- absolute addressing used in instructions and data
+-- no absolute addressing when compiling down
+-- fixed size allocator for multiples of 2 for zero external fragmentation
+
+-- an execute node is basically a list of AtomicInstruction
+-- a data node is basically a list of data 64 bits each
+-- instructions take data (from compute nodes or data nodes) and write to compute nodes and data nodes
+-- system IO is represented as data nodes I guess
+-- the more modular the nodes the better each specific subfield can be cached
+
+-- executeInstruction :: AtomicInstruction
+-- executeInstruction =
+
+-- I still dont know how you can run everything concurrently and "when the data is available"
+-- maybe it runs each time the data changes?
+
--- rei-hs/test/Spec.hs
+++ rei-hs/test/Spec.hs
@@ -7,8 +7,16 @@ main = runTestTTAndExit tests
 
 tests :: Test
 -- tests = TestList [ testParseParameterisedExpr ]
-tests = TestList []
+{--}
+tests = TestList [ "Sanity Foo" ~: (1, 2) ~=? foo 3 ]
 
 -- testParseParameterisedExpr :: Test
 -- doesnt work
 -- testParseParameterisedExpr = TestCase $ assertEqual "Parsing (a, b, c) should succeed" (Right ["(a, b, c)"]) (parse parameterisedExpr "" "(a, b, c)")
+
+{-| Sanity Foo
+
+>>> foo 3
+-}
+foo :: (Eq a1, Num a1) => a1 -> (a1, a1)
+foo 3 = (1, 2)
